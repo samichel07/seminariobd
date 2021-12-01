7c53bfe3f9f1659481c67415fa01d3084857b8ce
samichel07 <samichel1997girl@gmail.com>
2021-12-01 14:35:25 -0600

    Update insert_funcion.php
    Cambio en un insert

--- insert_funcion.php
+++ insert_funcion.php
@@ -7,7 +7,7 @@ $hora = $_POST["hora"];
 $teatro = implode((isset($_POST['teatro']) ? $_POST['teatro']: null));
 $obra=implode((isset($_POST['obra']) ? $_POST['obra']: null));
 $id_afinidad=implode((isset($_POST['afinidad']) ? $_POST['afinidad']: null));
-$sql = $db->query("INSERT into afinidad (id_funcion,fecha,hora,teatro,obra,id_afinidad)
+$sql = $db->query("INSERT into funcion (id_funcion,fecha,hora,teatro,obra,id_afinidad)
  values ('$id','$fecha ','$hora','$teatro','$obra','$id_afinidad')");
 
 if($sql){
