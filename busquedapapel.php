1447ee0da2dab81aa414b3a6d032ad4415984bfe
Viridiana_Rouge <94890002+15-vmr@users.noreply.github.com>
2021-12-07 15:57:55 -0600

    enlaces correctos en las busquedas

--- busquedapapel.php
+++ busquedapapel.php
@@ -43,14 +43,14 @@
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tabla Artista</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
+                            <li><i class="fa fa-table"></i><a href="agregar_artistas_form.php">Agregar nuevo artista</a></li>
                             <li><i class="fa fa-table"></i><a href="busquedaartista.php">Búsqueda de artistas</a></li>
                         </ul>
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tabla Autor</a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
+                            <li><i class="fa fa-table"></i><a href="agregar_autor_form.php">Agregar nuevo autor</a></li>
                             <li><i class="fa fa-table"></i><a href="busquedaautor.php">Búsqueda de autores</a></li>
                         </ul>
                     </li>
